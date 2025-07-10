9760556eacb3ed0a3383bc2ae896015d2d14e11a
AestheticShelby33 <altonmartin3333@gmail.com>
2025-07-10 16:26:10 +0530

    create a middleware and function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -18,9 +18,20 @@ const port =3000;
 
 app.use(express.json());
 
-app.use('/api/users', userRouter);
-app.use('/api/auth', authRouter);
+
 
 app.listen(port, ()=>{
     console.log('Server is running on port', port );
+});
+
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
