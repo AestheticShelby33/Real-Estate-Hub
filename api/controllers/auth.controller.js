e762d6495fa41aae4906719a9ec0a2f34327ab69
AestheticShelby33 <altonmartin3333@gmail.com>
2025-07-10 16:09:02 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs';
+
+export const signup = async(req, res)=>{
+    const {username, password, email} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({username, password: hashedPassword, email});
+    try{
+        await newUser.save();
+        res.status(201).json({message: "User created successfully", user: newUser});
+
+    }catch(error){
+        res.status(500).json({error: "Error creating user"})
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ const port =3000;
 app.use(express.json());
 
 app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(port, ()=>{
     console.log('Server is running on port', port );
